7b46e72c878d6e528d594bb42a4e48163c4df834
Tom Vautray <tom-vautray@hotmail.fr>
2024-03-20 22:52:38 +0100

    initializr

---
+++ src/main/java/com/theta/playlistr/PlaylistrApplication.java
@@ -0,0 +1,12 @@
+package com.theta.playlistr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistrApplication.class, args);
+	}
+}
