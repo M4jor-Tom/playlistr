3e7b0f6b5db54191f6e3b75c6d4d6f5d321cf9c7
Tom Vautray <tom-vautray@hotmail.fr>
2024-03-25 21:53:00 +0100

    Add artist/{artistName} endpoint

--- src/main/java/com/theta/playlistr/controller/ArtistController.java
+++ src/main/java/com/theta/playlistr/controller/ArtistController.java
@@ -1,7 +1,20 @@
 package com.theta.playlistr.controller;
 
+import com.theta.playlistr.domain.Artist;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ArtistController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("artist/{artistName}")
+    public Artist getArtistByName(@PathVariable("artistName") String artistName) {
+
+        return null;
+    }
 }
